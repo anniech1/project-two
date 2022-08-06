64be07f69bbd4bf8bcf87732fd6fd016a5dfa915
Kristina Faye <kglinoga@gmail.com>
2022-08-06 09:21:51 -0700

    populated server.js w/ boilerplate, added cont/api/userRoutes.js and added routes for getAll, add recipe and login

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+// for user Auth:
+const bcrypt = require("bcrypt");
+const { User,Gobble } = require('../../models');
+
+// to get all users, and includes their recipes
+// url: ${PORT}/api/users
+router.get("/",(req,res)=>{
+    // log msg to check if route works
+    console.log("Calling all Users, we're sharing recipes!");
+    // finding all the users
+    User.findAll({
+        include:[recipe]
+    }).then(data=>{
+        res.json(data)
+    }).catch(err=>{
+        res.status(500).json({msg:"ERROR",err})
+    })
+})
+
+// creating/adding a user
+// url: ${PORT}/api/users
+router.post("/",(req,res)=>{
+    // log msg to check if route works:
+    console.log("Welcome new user! Hope you're hungry!");
+
+    User.create({
+        // must check that this matches the format of USER MODEL
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password
+    }).then(data=>{
+        res.status(500).json({msg:"ERROR",})
+    })
+})
+
+// NOW we're logging someone in:
+// url: ${PORT}/api/users/login
+router.post("/login",(req,res)=>{
+    // log msg to check if route works
+    console.log("Welcome Back User! Missed you <3");
+
+    User.findOne({
+        where:{
+            email:req.body.email
+        }
+    }).then(foundUser=>{
+        // checking if there is a user by that email
+        if(!foundUser){
+                return res.status(401).json({msg:"who goes there?? (Invalid login credentials)"})
+        }
+        if(!bcrypt.compareSync(req.body.password,foundUser.password)){
+            return res.status(401).json({msg:"who goes there?? (Invalid login credentials)"})
+        }
+        return res.status(200).json(foundUser)
+    }).catch(err=>{
+        res.status(500).json({msg:"ERROR", err})
+    })
+})
+
+module.exports = router;
+
+
