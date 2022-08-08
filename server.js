f0509b61af8be8e4a736bba0b83d54dfcd8e355c
Annie Hosch <anniehosch@Annies-Air.lan>
2022-08-08 18:41:39 -0400

    add const hbs

--- server.js
+++ server.js
@@ -52,6 +52,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 const sess = {
   secret: 'Super secret secret',
