59fa3d4752ff1d9dc9167c7c14d81c9d5ae30aa8
Akshata Kumbhar <105023659+Akshu3011@users.noreply.github.com>
2022-08-10 16:48:14 -0700

    updated the code for region in post recipe

--- public/js/postRecipe.js
+++ public/js/postRecipe.js
@@ -2,12 +2,18 @@ let addIngredientsBtn = document.getElementById('addIngredientsBtn');
 let ingredientList = document.querySelector('.ingredientList');
 let ingredeintDiv = document.querySelectorAll('.ingredeintDiv')[0];
 
+
+var ingredientsArray=[];
 addIngredientsBtn.addEventListener('click', function(){
+  ingredientsArray.push(document.getElementById('ingredients').value);
   let newIngredients = ingredeintDiv.cloneNode(true);
   let input = newIngredients.getElementsByTagName('input')[0];
   input.value = '';
   ingredientList.appendChild(newIngredients);
 });
+
+
+
 
 
 
@@ -27,18 +33,14 @@ var myWidget = cloudinary.createUploadWidget({
 
         console.log("s" +imgurl);
 //selected region
-var option = ()=> {
-        var select = document.getElementById('region');
-				return select.options[select.selectedIndex].value;
+var region="";
+function getSelectValue(){
+        region = document.getElementById('region').value;
+			
 }
-        console.log(region); 
+  //      console.log(region); 
 
-        function checkAll() {  
-          var inputs = document.querySelectorAll('.dietary');   
-          for (var i = 0; i < inputs.length; i++) {   
-              inputs[i].checked = true;   
-          }   
-  }  
+    
   
   var checkboxes=document.querySelectorAll('.dietary');
   var listArray=[];
@@ -58,84 +60,18 @@ var option = ()=> {
       })
   }
  
-/*  function getCheckboxValue() {  
-    var checkboxes = 
-    document.getElementsByClassName('dietary');
 
-    var result = " ";
-  
-    for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            result= result +checkboxes[i].value + " "  ;
-            console.log("ak" ,result)
-            return result.toString();
-                ;
-        }
-    }
-    
-    
-  }/*
-
-
-
-
- 
-    
-    var l1 = document.getElementById("halal");  
-    var l2 = document.getElementById("vegan");  
-    var l3 = document.getElementById("vegetarian");  
-    var l4 = document.getElementById("gluten-free");  
-    var l5 = document.getElementById("dairy-free");  
-    var l6 = document.getElementById("sugar-free");  
-       
-    var res=" ";   
-    if (l1.checked == true){  
-      var dietary1 = document.getElementById("halal").value;  
-      res += dietary1 + ",";   
-    }   
-    else if (l2.checked == true){  
-      var dietary2 = document.getElementById("vegan").value;  
-       res += dietary2 + ",";   
-    }  
-    else if (l3.checked == true){  
-      
-      var dietary3 = document.getElementById("vegetarian").value;  
-      res += dietary3 + ",";   
-    }  
-    else if (l4.checked == true){  
-      var dietary4 = document.getElementById("gluten-free").value;  
-       res += dietary4 + ",";   
-    }  
-    else if (l5.checked == true){  
-      var dietary5 = document.getElementById("dairy-free").value;  
-      res += dietary5 + ",";   
-    }  
-    else if (l6.checked == true){  
-      var dietary6 = document.getElementById("sugar-free").value;  
-      res += dietary6;   
-    } else {  
-    return document.getElementById("result").innerHTML = "You have not selected anything";  
-    }  
-    return res;  
-  }  
-  */
-  
-  
 
 const addnewrecipe= (e) =>{
    
         e.preventDefault();
      
-          
-        
-        
-        //const ingredientsArr=document.querySelectorAll("#ingredients").value;
         console.log("submit");
         const recipeObj = {
             dish_name:document.querySelector("#dish_name").value,
             instructions:document.querySelector("#instructions").value,
-            ingredients:"ingredientsArr",
-            regions:"option",
+            ingredients:ingredientList.toString(),
+            regions:region,
             dietary:listArray.toString(),
             img_url:imgurl  
         }
